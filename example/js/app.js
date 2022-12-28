9ba138eb1750479580790a9b43fae8bec153cad3
Sanjar Saparov <117997805+sanzarr0704@users.noreply.github.com>
2022-12-28 15:44:09 +0600

    Это дз 5.1

--- example/js/app.js
+++ example/js/app.js
@@ -93,3 +93,131 @@ document.body.addEventListener('click', (e) => {
       closeModal()
   }
 })
+
+const  message = {
+    loading: "Идет загруска...",
+    success: "Спасибо скоро свяжимся!!!"
+
+}
+
+const form = document.querySelector("form")
+
+const  postData = (form) => {
+    form.addEventListener("sumbit", (e)=>{
+        e.preventDefault()
+
+          const request = new XMLHttpRequest()
+    request.open("POST" , "server.php")
+    request.setRequestHeader("Content-type"  , "application/json")
+
+    const  formData = new FormData(form)
+    const object = {}
+
+    formData.forEach((item) => {
+        obj[i] = item
+    })
+    console.log(object)
+
+        const json = JSON.stringify(object)
+
+        request.send(json)
+
+          request.addEventListener("load", ()=>{
+              if (request.status === 200){
+                  console.log("ok")
+                  messageBlock.textContent = message.success
+              } else {
+                  console.log("not ok")
+                   messageBlock.textContent = message.fail
+              }
+          })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const modal2 = document.querySelector(".modal")
+const modalTrigger2 = document.querySelector(".btn_dark")
+const closeModalBtn2 = document.querySelector(".modal__close")
+let isModalOpened2 = false
+
+const openModal2 = () => {
+    modal.classList.add("show")
+    modal.classList.remove("hide")
+    document.body.style.overflow = "hidden"
+}
+
+modalTrigger2.addEventListener("click", openModal)
+
+const closeModal2 = () => {
+    modal.classList.add("hide")
+    modal.classList.remove("show")
+    document.body.style.overflow = ""
+}
+
+modalTrigger2.addEventListener('click',openModal2)
+closeModalBtn2.addEventListener('click',closeModal2)
+
+window.onscroll =() => {
+    if (document.documentElement.scrollTop >= 3270 && isModalOpened === false) {
+        isModalOpened = true
+        openModal2()
+    }
+}
+document.body.addEventListener('click', (e) => {
+  if (e.target.classList.contains('show')) {
+      closeModal2()
+  }
+})
+
+const  message2 = {
+    loading: "Идет загруска...",
+    success: "Спасибо скоро свяжимся!!!"
+
+}
+
+const form2 = document.querySelector("form")
+
+const  postData2 = (form) => {
+    form.addEventListener("sumbit", (e)=>{
+        e.preventDefault()
+
+          const request = new XMLHttpRequest()
+    request.open("POST" , "server.php")
+    request.setRequestHeader("Content-type"  , "application/json")
+
+    const  formData = new FormData(form)
+    const object = {}
+
+    formData.forEach((item) => {
+        obj[i] = item
+    })
+    console.log(object)
+
+        const json = JSON.stringify(object)
+
+        request.send(json)
+
+          request.addEventListener("load", ()=>{
+              if (request.status === 200){
+                  console.log("ok")
+                  messageBlock.textContent = message.success
+              } else {
+                  console.log("not ok")
+                   messageBlock.textContent = message.fail
+              }
+          })
+    })
+}
+
+
+
